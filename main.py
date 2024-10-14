7aea1fe4be84e0efa8bd3b07be20a9942ac3e3de
ivk <leorivk@gmail.com>
2024-10-14 15:49:48 +0900

    Update exception handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
 
 
@@ -29,7 +29,7 @@ todo_data = {
 }
 
 
-@app.get("/todos")
+@app.get("/todos", status_code=200)
 def get_todos_handler(order: str | None = None):
     ret = list(todo_data.values())
     if order and order == "DESC":
@@ -37,9 +37,12 @@ def get_todos_handler(order: str | None = None):
     return ret
 
 
-@app.get("/todos/{todo_id}")
+@app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(todo_id: int):
-    return todo_data.get(todo_id, {}) # default value
+    todo = todo_data.get(todo_id)
+    if todo:
+        return todo
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 
 class CreateToDoRequest(BaseModel):
@@ -48,13 +51,13 @@ class CreateToDoRequest(BaseModel):
     is_done: bool
 
 
-@app.post("/todos")
+@app.post("/todos", status_code=201)
 def create_todo_handler(request: CreateToDoRequest):
     todo_data[request.id] = request.dict()
     return todo_data[request.id]
 
 
-@app.patch("/todos/{todo_id}")
+@app.patch("/todos/{todo_id}", status_code=200)
 def update_todo_handler(
         todo_id: int,
         is_done: bool = Body(..., embed=True),
@@ -63,11 +66,12 @@ def update_todo_handler(
     if todo:
         todo["is_done"] = is_done
         return todo
-    return {}
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 
-@app.delete("/todos/{todo_id}")
+@app.delete("/todos/{todo_id}", status_code=204)
 def delete_todo_handler(todo_id: int):
-    todo_data.pop(todo_id, None)
-    return todo_data
-
+    todo = todo_data.pop(todo_id, None)
+    if todo:
+        return
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
