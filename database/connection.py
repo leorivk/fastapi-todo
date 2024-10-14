3896fe8f53a055c7dca0ac0440bdccdae8ba2cc8
ivk <leorivk@gmail.com>
2024-10-14 16:21:26 +0900

    Update database connection

---
+++ database/connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:todo@127.0.0.1:3306/todo"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
