1859c9dca12450617cc52bff4970596efdd841f8
ivk <leorivk@gmail.com>
2024-10-14 15:43:08 +0900

    Update APIs

--- main.py
+++ main.py
@@ -1,7 +1,73 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def health_check_handler():
     return {"ping": "pong"}
+
+
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "FastAPI Todo App 1",
+        "is_done": True
+    },
+    2: {
+        "id": 2,
+        "contents": "FastAPI Todo App 2",
+        "is_done": False
+        },
+    3: {
+        "id": 3,
+        "contents": "FastAPI Todo App 3",
+        "is_done": False
+        }
+}
+
+
+@app.get("/todos")
+def get_todos_handler(order: str | None = None):
+    ret = list(todo_data.values())
+    if order and order == "DESC":
+        return ret[::-1]
+    return ret
+
+
+@app.get("/todos/{todo_id}")
+def get_todo_handler(todo_id: int):
+    return todo_data.get(todo_id, {}) # default value
+
+
+class CreateToDoRequest(BaseModel):
+    id: int
+    contents: str
+    is_done: bool
+
+
+@app.post("/todos")
+def create_todo_handler(request: CreateToDoRequest):
+    todo_data[request.id] = request.dict()
+    return todo_data[request.id]
+
+
+@app.patch("/todos/{todo_id}")
+def update_todo_handler(
+        todo_id: int,
+        is_done: bool = Body(..., embed=True),
+):
+    todo = todo_data.get(todo_id)
+    if todo:
+        todo["is_done"] = is_done
+        return todo
+    return {}
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo_handler(todo_id: int):
+    todo_data.pop(todo_id, None)
+    return todo_data
+
