d910af86c6802f765c770d189856c98bfc6ed325
ivk <leorivk@gmail.com>
2024-10-20 22:16:24 +0900

    Refactor (Repository Pattern)

--- database/repository.py
+++ database/repository.py
@@ -2,33 +2,40 @@
 
 from typing import List
 
+from fastapi import Depends
 from sqlalchemy import select, delete
 from sqlalchemy.orm import Session
 
+from database.connection import get_db
 from database.orm import ToDo
 
-def get_todos(session: Session) -> List[ToDo]:
-    return list(session.scalars(select(ToDo)))
+
+class ToDoRepository:
+    def __init__(self, session: Session = Depends(get_db)):
+        self.session = session
+
+    def get_todos(self) -> List[ToDo]:
+        return list(self.session.scalars(select(ToDo)))
 
 
-def get_todo_by_todo_id(session: Session, todo_id: int) -> ToDo | None:
-    return session.scalar(select(ToDo).where(ToDo.id == todo_id))
+    def get_todo_by_todo_id(self, todo_id: int) -> ToDo | None:
+        return self.session.scalar(select(ToDo).where(ToDo.id == todo_id))
 
 
-def create_todo(session: Session, todo: ToDo) -> ToDo:
-    session.add(instance=todo)
-    session.commit()    # db에 저장 (Autocommit을 False로 지정했으므로 명시적으로 커밋)
-    session.refresh(instance=todo)  # 저장된 todo 객체를 다시 가져온 후 todo_id 반영
-    return todo # 데이터베이스에 완전히 반영된 todo 객체를 반환
+    def create_todo(self, todo: ToDo) -> ToDo:
+        self.session.add(instance=todo)
+        self.session.commit()    # db에 저장 (Autocommit을 False로 지정했으므로 명시적으로 커밋)
+        self.session.refresh(instance=todo)  # 저장된 todo 객체를 다시 가져온 후 todo_id 반영
+        return todo # 데이터베이스에 완전히 반영된 todo 객체를 반환
 
 
-def update_todo(session: Session, todo: ToDo) -> ToDo:
-    session.add(instance=todo)
-    session.commit()
-    session.refresh(instance=todo)
-    return todo
+    def update_todo(self, todo: ToDo) -> ToDo:
+        self.session.add(instance=todo)
+        self.session.commit()
+        self.session.refresh(instance=todo)
+        return todo
 
 
-def delete_todo(session: Session, todo_id: int) -> None:
-    session.execute(delete(ToDo).where(ToDo.id == todo_id))
-    session.commit()
+    def delete_todo(self, todo_id: int) -> None:
+        self.session.execute(delete(ToDo).where(ToDo.id == todo_id))
+        self.session.commit()
